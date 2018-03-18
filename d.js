49eb41c44976ced17bb68c64b4477e61b7c47d80
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-18 11:55:55 +0700

    xong

---
+++ d.js
@@ -0,0 +1,19 @@
+// function veHinh1(n) {
+//     let s = '';
+//     for (let i = 0; i < n; i++) {
+//         console.log(s += '*');
+//     }
+// }
+
+// veHinh1(5);
+
+console.log(5 == '5');
+console.log(5 != '5');
+console.log(5 === '5');
+console.log(5 !== '5');
+
+// console.log('aaa' === 'aaa');
+// console.log(!null);
+
+let a = 5;
+console.log((a = 6) * 6);
---
+++ e.js
@@ -0,0 +1,24 @@
+function inSoChan() {
+    for(let i = 0; i <= 100; i += 2) {
+        // if (i % 2 === 0) console.log(i);
+        console.log(i);
+    }
+}
+
+function inSoLe() {
+    for(let i = 1; i <= 100; i += 2) console.log(i);
+}
+
+function inSoChia3Du1() {
+    for(let i = 1; i <= 100; i += 3) console.log(i);
+}
+function inSoChinhPhuong() {
+    for(let i = 0; i <= 10; i++) console.log(i * i);
+}
+
+inSoChinhPhuong();
+
+// inSoChia3Du1();
+
+// Math.sqrt()
+// const x = 1;
